7bf40e9cd3a7a144f346b7b2688e8ed341db7bd4
joaostr22 <joaodgrodrigues22@gmail.com>
2025-07-16 05:08:31 +0000

    deploy app 1st try

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles.csv') # lendo os dados
+
+st.header('Título provisório')
+
 hist_button = st.button('Criar histograma') # criar um botão
+
+scatter_button = st.button('Criar gráfico de dispersão')
 
 if hist_button: # se o botão for clicado       
     # escrever uma mensagem
@@ -14,3 +19,10 @@ if hist_button: # se o botão for clicado
         
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button:
+    st.write('Criando um gráfico de dispersão')
+
+    fig2 = px.scatter(car_data, x='odometer', y='price')
+
+    st.plotly_chart(fig2, use_container_width=True)
