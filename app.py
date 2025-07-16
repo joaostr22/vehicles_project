e0519456f5a8da6cf6e49fc7f26b8ad63462f62c
joaostr22 <joaodgrodrigues22@gmail.com>
2025-07-16 22:04:07 +0000

    deploy app v2

--- app.py
+++ app.py
@@ -2,27 +2,48 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles.csv') # lendo os dados
+car_data = pd.read_csv('vehicles.csv')
 
-st.header('Título provisório')
+st.header('Análise de venda de carros')
 
-hist_button = st.button('Criar histograma') # criar um botão
+hist_button = st.checkbox('Criar histograma')
 
-scatter_button = st.button('Criar gráfico de dispersão')
+scatter_button = st.checkbox('Criar gráfico de dispersão')
 
-if hist_button: # se o botão for clicado       
-    # escrever uma mensagem
-    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-            
-    # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
-        
-    # exibir um gráfico Plotly interativo
+if hist_button:
+    st.write('Criando um histograma para analisar o ano do modelo por condição')
+
+    fig = px.histogram(
+    car_data,
+    x='model_year',
+    color='condition',
+    barmode='stack',
+    title='Distribuição de condição dos veículos por ano do modelo'
+    )
+
+    fig.update_layout(
+    xaxis_title='Ano do Modelo',
+    yaxis_title='Quantidade de Veículos',
+    legend_title='Condição'
+    )
+
     st.plotly_chart(fig, use_container_width=True)
 
 if scatter_button:
-    st.write('Criando um gráfico de dispersão')
+    st.write('Criando um gráfico de dispersão para analisar como o preço de um veículo varia com a quilometragem (odômetro) e o tipo de câmbio.')
 
-    fig2 = px.scatter(car_data, x='odometer', y='price')
+    fig2 = px.scatter(
+        car_data,
+        x='odometer',
+        y='price',
+        color='transmission',
+        title='Preço VS quilometragem por tipo de câmbio'
+    )
+
+    fig2.update_layout(
+        xaxis_title='Quilometragem',
+        yaxis_title='Preço',
+        legend_title='Tipo de câmbio'
+    )
 
     st.plotly_chart(fig2, use_container_width=True)
